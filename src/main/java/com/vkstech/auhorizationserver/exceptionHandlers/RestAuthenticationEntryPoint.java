9180224e2f16c8c0dcccbdc22ce21cd9f5eead8a
Vipul Kumar Singh <vipul24.9669@gmail.com>
2019-08-17 16:33:32 +0530

    Status code changed in AuthenticationEntryPoint

--- src/main/java/com/vkstech/auhorizationserver/exceptionHandlers/RestAuthenticationEntryPoint.java
+++ src/main/java/com/vkstech/auhorizationserver/exceptionHandlers/RestAuthenticationEntryPoint.java
@@ -6,6 +6,7 @@ import com.vkstech.auhorizationserver.utils.ResponseObject;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,6 +19,8 @@ public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException {
         ResponseObject response = new ResponseObject(ResponseMessages.INVALID_TOKEN);
+        httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        httpServletResponse.setContentType(MappingJackson2JsonView.DEFAULT_CONTENT_TYPE);
         OutputStream out = httpServletResponse.getOutputStream();
         ObjectMapper mapper = new ObjectMapper();
         mapper.writeValue(out, response);
